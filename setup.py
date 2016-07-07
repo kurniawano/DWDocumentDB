93d774e59dee2027db99baf88cdd1dacca22d59c
Oka Kurniawan <kurniawano@ieee.org>
2016-07-07 11:17:23 +0800

    Modify some config file.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from os import path
 here=path.abspath(path.dirname(__file__))
 
 #get the long description from the README file 
-with open(path.join(here,'README.rst'),encoding='utf-8') as f:
+with open(path.join(here,'README.md'),encoding='utf-8') as f:
 	long_description=f.read()
 
 setup(
 	name='DWDocumentDB',
-	version='1.0.0',
+	version='1.0.0b1',
 	description='Python library wrapper for pydocumentdb',
 	long_description=long_description,
 	url='https://github.com/kurniawano/DWDocumentDB',
@@ -18,7 +18,7 @@ setup(
 	author_email='kurniawano@ieee.org',
 	license='MIT',
 	classifiers=[
-	'Development STatus :: 4 - Beta',
+	'Development Status :: 4 - Beta',
 	'Intended Audience :: Education',
 	'license :: OSI Approved :: MIT license',
 	'Programming Language :: PYthon :: 2.7'
